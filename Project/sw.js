dd6ffad150b8dde1c86774e8e3a937f991af682c
beyza <70069234+beyzasahingoz@users.noreply.github.com>
2022-12-29 13:36:34 +0300

    Update sw.js

--- Project/sw.js
+++ Project/sw.js
@@ -1,10 +1,13 @@
 self.addEventListener("install", e => {
     e.waitUntil(
         caches.open("static").then(cache => {
-            return cache.addAll(["./", "./style.css", "./img/pic1.png"]);
+            return cache.addAll(["./", "./style.css", "./img/pic2.png"]).catch(error => {
+                console.log(error);
+            });
         })
     );
 });
+
 self.addEventListener("fetch", e => {
     e.respondWith(
         caches.match(e.request).then(response => {
